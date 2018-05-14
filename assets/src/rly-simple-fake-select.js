ae0901d4752d93ebc628a205af5b65868e9c1512
Kiliano Lopes <kiliano.lopes@degraupublicidade.com.br>
2018-05-14 11:13:04 -0300

    funcional js

--- assets/src/rly-simple-fake-select.js
+++ assets/src/rly-simple-fake-select.js
@@ -1,19 +1,41 @@
 $(function(){
 
-	// Define CSS Prefix
+	// Define Class Prefix
 	var rlySimplePrefix = "rly-simple";
+
+
 
 	$("."+rlySimplePrefix+"-select").each(function(){
 		var rlySimpleContent = $(this).html().replace(/<option/g,"<li><a href='javascript:void(0)'");
 		var rlySimpleContent = rlySimpleContent.replace(/option>/g,"a></li>");
 		var rlySimpleFirstOption = $(this).find("option:first-child").text();
 
-		$(this).after("<span class='"+rlySimplePrefix+"-fake-select'><a href='javascript:void(0)'>"+rlySimpleFirstOption+"</a><ul class='"+rlySimplePrefix+"-fake-options'>"+rlySimpleContent+"</ul></span>");
+		// Aparence
+
+		var rlySimpleWidth = $(this).width();
+
+		alert(rlySimpleWidth);
+		
+
+		$(this).after("<span class='"+rlySimplePrefix+"-fake-select'><a href='javascript:void(0)' class='"+rlySimplePrefix+"-fake-link' style='width: "+rlySimpleWidth+"px;'>"+rlySimpleFirstOption+"</a><ul class='"+rlySimplePrefix+"-fake-options'>"+rlySimpleContent+"</ul></span>");
+		
 	});
 
 
 	$("."+rlySimplePrefix+"-fake-select > a").click(function(){
-		$(this).next(".rly-simple-fake-options").slideToggle();
+		$(this).next("."+rlySimplePrefix+"-fake-options").toggle();
+	});
+
+	$("."+rlySimplePrefix+"-fake-options > li > a").click(function(){
+		var rlySimpleOptionSelect = $(this).text();
+		var rlySimpleOptionSelectValue = $(this).attr("value");
+
+		$(this).parents("."+rlySimplePrefix+"-fake-select").prev("select").val(rlySimpleOptionSelectValue);
+
+		$(this).parents("."+rlySimplePrefix+"-fake-options").find("li > a").removeClass(rlySimplePrefix+"-active");
+		$(this).addClass(rlySimplePrefix+"-active");
+		$(this).parents("."+rlySimplePrefix+"-fake-select").find("."+rlySimplePrefix+"-fake-link").html(rlySimpleOptionSelect);
+		$(this).parents("."+rlySimplePrefix+"-fake-options").hide();
 	});
 
 	
