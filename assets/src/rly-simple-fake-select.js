f3ef66bdbf703a4e03ea8b588d10fd93592d727e
Kiliano Lopes <kiliano.lopes@degraupublicidade.com.br>
2018-05-14 17:10:31 -0300

    Escolha feita com Select ou com value null

--- assets/src/rly-simple-fake-select.js
+++ assets/src/rly-simple-fake-select.js
@@ -8,15 +8,24 @@ $(function(){
 	$("."+rlySimplePrefix+"-select").each(function(){
 		var rlySimpleContent = $(this).html().replace(/<option/g,"<li><a href='javascript:void(0)'");
 		var rlySimpleContent = rlySimpleContent.replace(/option>/g,"a></li>");
-		var rlySimpleFirstOption = $(this).find("option:first-child").text();
 
-		// Aparence
+		// Selected Option
+		var rlySimpleSelectedOption = $(this).val();
+		var rlySimpleSelectedOptionTxt = $(this).find("option[value='"+rlySimpleSelectedOption+"']").text();
 
+		if (rlySimpleSelectedOptionTxt == "" || rlySimpleSelectedOptionTxt == null) {
+			var rlySimpleSelectedOptionTxt = $(this).val();
+		}
+
+		// Size
 		var rlySimpleWidth = $(this).css("width");
 		var rlySimpleHeight = $(this).css("height");
 
+		// Take true select class
+		var rlySimpleClass = $(this).attr("class").replace(rlySimplePrefix+'-select',"");
 
-		$(this).after("<span class='"+rlySimplePrefix+"-fake-select'><a href='javascript:void(0)' class='"+rlySimplePrefix+"-fake-link' style='width: "+rlySimpleWidth+"; height: "+rlySimpleHeight+";'>"+rlySimpleFirstOption+"</a><ul class='"+rlySimplePrefix+"-fake-options'>"+rlySimpleContent+"</ul></span>");
+		// Building Fake Select
+		$(this).after("<span class='"+rlySimplePrefix+"-fake-select'><a href='javascript:void(0)' class='"+rlySimplePrefix+"-fake-link "+rlySimpleClass+"' style='width: "+rlySimpleWidth+"; height: "+rlySimpleHeight+";'>"+rlySimpleSelectedOptionTxt+"</a><ul class='"+rlySimplePrefix+"-fake-options'>"+rlySimpleContent+"</ul></span>");
 		
 	});
 
@@ -30,6 +39,10 @@ $(function(){
 		var rlySimpleOptionSelect = $(this).text();
 		var rlySimpleOptionSelectValue = $(this).attr("value");
 
+		if (rlySimpleOptionSelectValue == "" || rlySimpleOptionSelectValue == null ) {
+			var rlySimpleOptionSelectValue = $.trim(rlySimpleOptionSelect);
+		}
+		
 		$(this).parents("."+rlySimplePrefix+"-fake-select").prev("select").trigger("change").val(rlySimpleOptionSelectValue);
 
 		$(this).parents("."+rlySimplePrefix+"-fake-options").find("li > a").removeClass(rlySimplePrefix+"-active");
@@ -41,6 +54,7 @@ $(function(){
 	// Clicking on true
 	$("."+rlySimplePrefix+"-select").change(function(){
 		var rlySimpleContentTrueValue = $(this).val();
+
 		var rlySimpleContent = $(this).next("."+rlySimplePrefix+"-fake-select").find("."+rlySimplePrefix+"-fake-options > li > a[value='"+rlySimpleContentTrueValue+"']").text();
 		$(this).next("."+rlySimplePrefix+"-fake-select").find("."+rlySimplePrefix+"-fake-link").text(rlySimpleContent);
 	});
