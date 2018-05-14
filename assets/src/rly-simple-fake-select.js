20d1bc87ffd1e105c74c525707c4658e62b9651f
Kiliano Lopes <kiliano.lopes@degraupublicidade.com.br>
2018-05-14 10:39:11 -0300

    js and css tests

--- assets/src/rly-simple-fake-select.js
+++ assets/src/rly-simple-fake-select.js
@@ -1,15 +1,19 @@
 $(function(){
 
 	// Define CSS Prefix
-	var rlySimplePrefix = "rly-simple-";
+	var rlySimplePrefix = "rly-simple";
 
-	$("."+rlySimplePrefix+"select").each(function(){
-		var rlySimpleContent = $(this).html().replace(/option/g,"li");
+	$("."+rlySimplePrefix+"-select").each(function(){
+		var rlySimpleContent = $(this).html().replace(/<option/g,"<li><a href='javascript:void(0)'");
+		var rlySimpleContent = rlySimpleContent.replace(/option>/g,"a></li>");
 		var rlySimpleFirstOption = $(this).find("option:first-child").text();
 
-		$(this).after("<a href='javascript:void(0)' class='"+rlySimplePrefix+"fake-select'>"+rlySimpleFirstOption+"</a><ul class='"+rlySimplePrefix+"fake-options'>"+rlySimpleContent+"</ul>");
+		$(this).after("<span class='"+rlySimplePrefix+"-fake-select'><a href='javascript:void(0)'>"+rlySimpleFirstOption+"</a><ul class='"+rlySimplePrefix+"-fake-options'>"+rlySimpleContent+"</ul></span>");
+	});
 
 
+	$("."+rlySimplePrefix+"-fake-select > a").click(function(){
+		$(this).next(".rly-simple-fake-options").slideToggle();
 	});
 
 	
